8cc4a3ded3e9440c3e4c319b8364f3ebd540fb1c
Folasayo Samuel <folasayosamuel@gmail.com>
2022-10-20 10:42:56 +0100

    update

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace EnterOnlyNumbersUsingDoWhileLoop;
+
+public class Program
+{
+	static void Main(string[]args)
+	{
+		double val = 0;
+		string num = "";
+		Console.Write("Enter Number: ");
+		ConsoleKeyInfo chr;
+		do
+		{
+			chr = Console.ReadKey(true);
+			if (chr.Key != ConsoleKey.Backspace)
+			{
+				bool control = double.TryParse(chr.KeyChar.ToString(), out val);
+				if (control)
+				{
+					num += chr.KeyChar;
+					Console.Write(chr.KeyChar);
+					Console.Write("\b \b");
+				}
+			}
+		}
+		while (chr.Key != ConsoleKey.Enter);
+        Console.ReadKey();
+    }
+}
